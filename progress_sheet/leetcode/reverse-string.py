346d407d96470f3f901ec41ecd6695eac4ed3c17
saleamlaka2 <160355563+saleamlaka2@users.noreply.github.com>
2024-02-27 11:54:27 +0300

    Add solution for Reverse String

---
+++ progress_sheet/leetcode/reverse-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def reverse(l,r):
+            if l>=r:
+                return 
+            s[l],s[r]=s[r],s[l]
+            reverse(l+1,r-1)
+        reverse(0,len(s)-1)
+        
