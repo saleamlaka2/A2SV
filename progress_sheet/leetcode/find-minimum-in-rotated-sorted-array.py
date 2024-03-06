2fca3422226177c11a07fa97a9ee6fb10ab704f9
saleamlaka2 <160355563+saleamlaka2@users.noreply.github.com>
2024-03-06 15:16:18 +0300

    Add solution for Find Minimum in Rotated Sorted Array

--- progress_sheet/leetcode/find-minimum-in-rotated-sorted-array.py
+++ progress_sheet/leetcode/find-minimum-in-rotated-sorted-array.py
@@ -5,16 +5,16 @@ class Solution:
                 if nums[m]>nums[m+1]:
                     return True
             return False
+        if nums[0]<=nums[-1]:
+            return nums[0]
         low=0
         high=len(nums)-1
 
         while low<=high:
             mid=(low+high)//2
-            # print(mid,check(mid))
             if check(mid):
                 return nums[mid+1]
             elif nums[mid]<nums[0]:
                 high=mid-1
             else:
                 low=mid+1
-        return nums[0]
